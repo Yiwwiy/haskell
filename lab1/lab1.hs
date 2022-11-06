13227bd52a2d4d6b1fe8ce32c1a7a1e6c85ead7a
Yiwwiy <114854690+Yiwwiy@users.noreply.github.com>
2022-11-06 21:40:00 +0500

    Update lab1.hs

--- lab1/lab1.hs
+++ lab1/lab1.hs
@@ -1,11 +1,11 @@
---task 2
+--task 2 функция НОД
 gcd' :: Int -> Int -> Int
 gcd' x 0 = x 
-gcd' x y = gcd' y (x `rem` y)
+gcd' x y = gcd' y (x `rem` y)  -- `rem` "выкидывает" знак агрумента
 
 
 
---task 3
+--task 3 фукция проверки числа на простоту
 --1 вариант
 divisible :: Int->[Int]->Bool
 divisible x y
@@ -26,14 +26,14 @@ prime n
 --2 вариант          
 prime' n = n == 2 || (n > 2  &&  odd n  &&  [k | k <- [3,5 .. n-2], n `mod` k == 0] == [])
 
- --task 4
+ --task  функция числа-перевёртыша
 reverseNumber:: Integer->Integer
 reverseNumber n = rev 0 n
     where 
         rev x 0 = x
         rev x n = rev (x * 10 + mod n 10) (div n 10)
 
--- task 4
+-- task 4 -- функция, которая находит наибольший корень билинейного уравнения
 functionRoot :: Int -> Int -> Int -> Double  
 functionRoot a b c = if d < 0 then 0/0 else (max x1 x2)
                         where
@@ -42,7 +42,7 @@ functionRoot a b c = if d < 0 then 0/0 else (max x1 x2)
                           d = fromIntegral (b * b - 4 * a * c)
                           e = - fromIntegral b / (2 * fromIntegral a)
 
---task 5
+--task 5 метод бисекции
 maxRoot :: Int -> Int -> Int -> Double
 maxRoot a b c = 
   let 
